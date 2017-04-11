437066649066d687bcd8347180d01d8728be31d5
Caique Felix <caiquefgusmao@gmail.com>
2017-04-11 12:29:14 -0300

    Adicionamos a pagina Exclusao

---
+++ crud-webforms/crud-webforms/Exclusao.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Exclusao.aspx.cs" Inherits="crud_webforms.Exclusao" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+		<div>
+			<asp:Label Text="" runat="server" ID="lblMsg" />
+		</div>
+		<div>
+			<asp:Button Text="Sim" runat="server" ID="btnSim" OnClick="btnSim_Click" />
+			<asp:Button Text="Não" runat="server" ID="btnNao" OnClick="btnNao_Click" />
+		</div>
+	</div>
+    </form>
+</body>
+</html>
---
+++ crud-webforms/crud-webforms/Exclusao.aspx.cs
@@ -0,0 +1,89 @@
+﻿using crud_webforms.Utils;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace crud_webforms
+{
+	public partial class Exclusao : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (IsPostBack == false)
+			{
+				int id;
+				if (int.TryParse(Request.QueryString["id"], out id) == false)
+				{
+					lblMsg.Text = "Id inválido!";
+					btnSim.Visible = false;
+					btnNao.Text = "Voltar";
+					return;
+				}
+
+				// Cria e abre a conexão com o banco de dados
+				using (SqlConnection conn = Sql.OpenConnection())
+				{
+
+					// Cria um comando para selecionar registros da tabela
+					using (SqlCommand cmd = new SqlCommand("SELECT Nome FROM tbPessoa WHERE Id = @id", conn))
+					{
+						cmd.Parameters.AddWithValue("@id", id);
+
+						using (SqlDataReader reader = cmd.ExecuteReader())
+						{
+							// Tenta obter o registro
+							if (reader.Read() == true)
+							{
+								lblMsg.Text = "Deseja mesmo excluir " + reader.GetString(0) + "?";
+							}
+							else
+							{
+								lblMsg.Text = "Id não encontrado!";
+								btnSim.Visible = false;
+								btnNao.Text = "Voltar";
+							}
+						}
+					}
+				}
+			}
+		}
+
+		protected void btnSim_Click(object sender, EventArgs e)
+		{
+			int id;
+			if (int.TryParse(Request.QueryString["id"], out id) == false)
+			{
+				lblMsg.Text = "Id inválido!";
+				btnSim.Visible = false;
+				btnNao.Text = "Voltar";
+				return;
+			}
+
+			// Cria e abre a conexão com o banco de dados
+			using (SqlConnection conn = Sql.OpenConnection())
+			{
+
+				// Cria um comando para excluir o registro
+				using (SqlCommand cmd = new SqlCommand("DELETE FROM tbPessoa WHERE Id = @id", conn))
+				{
+					cmd.Parameters.AddWithValue("@id", id);
+
+					cmd.ExecuteNonQuery();
+				}
+			}
+
+			lblMsg.Text = "Pessoa excluída com sucesso!";
+			btnSim.Visible = false;
+			btnNao.Text = "Voltar";
+		}
+
+		protected void btnNao_Click(object sender, EventArgs e)
+		{
+			Response.Redirect("Listagem.aspx");
+		}
+	}
+}
