dcb69e5d6971be3e5149da7c42ecebbffcb5fe1d
Caique Felix <caiquefgusmao@gmail.com>
2017-04-11 10:59:06 -0300

    Criamos a model Pessoa

---
+++ crud-webforms/crud-webforms/Models/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace crud_webforms.Models
+{
+	public class Pessoa
+	{
+		public int Id { get; set; }
+
+		public string Nome { get; set; }
+
+		public string Endereco { get; set; }
+
+		public string Email { get; set; }
+
+		public DateTime Nascimento { get; set; }
+
+		public double Peso { get; set; }
+	}
+}
